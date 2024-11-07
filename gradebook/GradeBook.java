b7ce600f0917944df7beba236a6ad1cdb750d724
dustij <knightlybyte@gmail.com>
2024-11-07 13:04:12 -0600

    Prints students list, changes a students grade

--- gradebook/GradeBook.java
+++ gradebook/GradeBook.java
@@ -16,22 +16,36 @@ public class GradeBook {
 
         System.out.println("Welcome to the CM111 Grade Book App!");
 
-        while(true) {
+        while (true) {
             System.out.println("\nPlease make a selection:\n");
             System.out.println("1) List Class Grades");
-            System.out.println("2) Exit");
+            System.out.println("2) Update a Student's Grade");
+            System.out.println("3) Exit");
             System.out.print("\nPlease choose an option: ");
             String choice = input.nextLine();
             System.out.println();
-            switch(choice) {
-                case "1": // TODO: write a loop that, for each student prints 
-                          //       lname, fname: grade 
+            switch (choice) {
+                case "1":
+                    // print each student -> lname, fname: grade
+                    for (int i = 0; i < students.length; i++) {
+                        System.out.printf("%-15s: %6.2f%n",
+                                students[i].getLastName() + ", " + students[i].getFirstName(),
+                                students[i].getGrade());
+                    }
                     continue;
                 case "2":
+                    // update grades for selected student
+                    System.out.printf("Choose a student [1-%d]: ", students.length);
+                    int id = input.nextInt() - 1;
+                    System.out.println(
+                            "Updating grade for " + students[id].getFirstName() + " " + students[id].getLastName());
+                    System.out.println("Current grade is: " + students[id].getGrade());
+                    System.out.print("Enter a new grade: ");
+                    students[id].setGrade(input.nextDouble());
+                    continue;
+                case "3":
                     System.out.println("Goodbye!");
                     return;
-
-                // Challenge: write another option to allow updating grades for a selected student
             }
         }
     }
